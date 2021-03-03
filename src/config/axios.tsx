34526ab29ca9421c0b1d6b54e25cdd97612b675b
Nathan Chacon <nathanchacon321@gmail.com>
2021-03-03 00:36:02 -0300

    Add my own proxy

--- src/config/axios.tsx
+++ src/config/axios.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export default axios.create({
-  baseURL: 'https://thingproxy.freeboard.io/fetch/https://jobs.github.com/',
+  baseURL: 'https://cors-anywhere-nathan-chacon.herokuapp.com/https://jobs.github.com/',
   headers: {
     'Content-Type': 'application/json',
   }
