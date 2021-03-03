4c17cd75dab1841c8ea1863b72d2ea380bf1c5cd
Nathan Chacon <nathanchacon321@gmail.com>
2021-03-03 00:07:16 -0300

    Try fix cors

--- src/config/axios.tsx
+++ src/config/axios.tsx
@@ -1,4 +1,7 @@
 import axios from 'axios';
 export default axios.create({
-  baseURL: 'https://thingproxy.freeboard.io/fetch/https://jobs.github.com/'
+  baseURL: 'https://thingproxy.freeboard.io/fetch/https://jobs.github.com/',
+  headers: {
+    'Content-Type': 'application/json',
+  }
 })
